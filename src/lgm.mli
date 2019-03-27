3229ac6013465ed872a35f6a41b1e6bf7114e5e4
Ta-Chu Kao <tck@cued-wifi-213-102.eng.cam.ac.uk>
2019-03-27 19:02:00 +0000

    added .mli

---
+++ src/lgm.mli
@@ -0,0 +1,20 @@
+open Owl
+
+(** negloglik data w sigma *)
+val negloglik :
+  Mat.mat ->
+  Mat.mat ->
+  Mat.mat ->
+  float 
+
+(** infer ?verbose=true ?(tol=1E-5) ?(every=10) ?(max_steps=1000) ?(model=`fa) data d *)
+val infer :
+  ?verbose:bool -> 
+  ?tol:float -> 
+  ?every:int -> 
+  ?max_steps:int -> 
+  ?model:[`fa | `ppca] ->
+  Mat.mat -> 
+  int -> 
+  Mat.mat * Mat.mat * Mat.mat * Mat.mat * float
+
